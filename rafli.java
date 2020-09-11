bf47e86eea73133e698883c9900d2755036f1f6f
raflifik <60343163+raflifik@users.noreply.github.com>
2020-09-11 14:28:42 +0700

    Add files via upload

---
+++ rafli.java
@@ -0,0 +1,28 @@
+package kipli;
+
+public class rafli {
+
+  public static void main(String[] args) {
+  String R = "ABCDEFGHIJKLMNOPQRSTUVWXYZ#-";
+  char a = R.charAt(17);
+  char b = R.charAt(0);
+  char c = R.charAt(5);
+  char d = R.charAt(11);
+  char e = R.charAt(8);
+  char f = R.charAt(27);
+  char g = R.charAt(25);
+  char h = R.charAt(0);
+  char i = R.charAt(8);
+  char j = R.charAt(13);
+  char k = R.charAt(0);
+  char l = R.charAt(11);
+  char m = R.charAt(27);
+  char n = R.charAt(5);
+  char o = R.charAt(8);
+  char p = R.charAt(10);
+  char q = R.charAt(17);
+  char r = R.charAt(8);
+  System.out.println(a+""+b+""+c+""+d+""+e+""+f+""+g+""+h+""+i+""+j+""+k+""+l+""+m+""+n+""+o+""+p+""+q+""+r);
+  
+  }
+}
